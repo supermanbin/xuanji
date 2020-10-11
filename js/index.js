7228144d9d8661fdc1b2f6d0a69eb20cfc2fa83c
dabin <fengbinsuperman@gmail.com>
2020-10-11 22:38:43 +0800

    home page

--- js/index.js
+++ js/index.js
@@ -1,35 +1,40 @@
 $(function () {
-  $('.banner').slick({
-    autoplay: true,
-    accessibility: false,
-    dots: true,
-    draggable: false,
-    arrows: false,
-  });
-  $('.tab-banner').slick({
-    autoplay: true,
-    accessibility: false,
-    dots: true,
-    dotsClass: 'tab-banner-dots slick-dots',
-    draggable: false,
-    arrows: false,
-  });
+  var player = new Plyr('#player');
+  var bannerSlick = $('.banner')
+    .slick({
+      autoplay: true,
+      accessibility: false,
+      dots: false,
+      draggable: false,
+      arrows: true,
+      cssEase: 'cubic-bezier(0.22, 0.61, 0.36, 1)',
+      speed: 900,
+    })
+    .on('afterChange', function (event, slick) {
+      console.log(event);
+      // left
+    });
 
-  $('.activity-notice-banner').slick({
+  var w = $('.banner-item').width();
+  $('.banner-infos').css('width', w + 'px');
+
+  // $('.parameter__item').slick({
+  //   autoplay: true,
+  //   accessibility: false,
+  //   dots: true,
+  //   dotsClass: 'tab-banner-dots slick-dots',
+  //   draggable: false,
+  //   arrows: false,
+  // });
+
+  $('.parameter-list').slick({
     autoplay: true,
     accessibility: false,
     dots: true,
-    // dotsClass: 'tab-banner-dots slick-dots',
+    dotsClass: 'parameter-list-dots slick-dots',
     draggable: false,
     arrows: false,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-  });
-
-  $('[data-toggle="datepicker"]').datepicker({
-    autoShow: true,
-    inline: true,
-    container: '',
-    language: 'zh-CN',
+    slidesToShow: 5,
+    slidesToScroll: 5,
   });
 });
